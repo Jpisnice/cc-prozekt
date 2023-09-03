969503cd3377eb760fd5a8e76ce63b6d06d897cc
Jpisnice <108455222+Jpisnice@users.noreply.github.com>
2023-09-03 17:43:31 +0530

    hi

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,21 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDhQ9Xiszm5nG1r0MhSMtjrbpRzPd_wXjI",
-    authDomain: "moviz-c49c5.firebaseapp.com",
-    projectId: "moviz-c49c5",
-    storageBucket: "moviz-c49c5.appspot.com",
-    messagingSenderId: "243493721479",
-    appId: "1:243493721479:web:01281e10b66b8106946f22",
-    measurementId: "G-FDGP4YWK7Q"
+    apiKey: "AIzaSyD37MTUUiQ2g1jdPRigN3vX_2DwHKxlQ20",
+    authDomain: "cloudcomputing-81292.firebaseapp.com",
+    projectId: "cloudcomputing-81292",
+    storageBucket: "cloudcomputing-81292.appspot.com",
+    messagingSenderId: "146214611497",
+    appId: "1:146214611497:web:e0369673cf58918c03ca38",
+    measurementId: "G-09PFV472CQ"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+
+// Export the initialized Firebase app and auth
+export { app, auth };
+
+
